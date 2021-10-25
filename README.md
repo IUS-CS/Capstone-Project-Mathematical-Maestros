384f339410ce5e2560a7dcd7846c64c756674e00
jmhasty <jmhasty@iu.edu>
2021-10-24 23:08:58 -0400

    database changes and user sign_in/ register

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Capstone-Project-Mathematical-Maestros
+## Getting Started
+---
+
+### Install Magenta
+<https://github.com/magenta/magenta>
+
+---
+```
+# Create Virtual Environment
+$ python -m venv venv
+
+# Activate Virtual Environment (Linux)
+$ source venv/bin/activate
+
+# Install dependencies
+$ pip install -r requirements.txt
+
+# Create DB
+$ python
+>> from server import db
+>> db.create_all()
+>> exit()
+
+# Run Server (http://127.0.0.1:5000)
+python server.py
+```
+
+## Endpoints
+--- 
+
+* GET       /song
+* GET       /song/:id
+* POST      /song
+* PUT       /song/:id
+* DELETE    /song/:id
+
+* GET, POST /sign_in
+* GET, POST /register
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,8 @@
-
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy 
-from flask_marshmallow import Marshmallow 
+from flask_marshmallow import Marshmallow
+from sqlalchemy.orm import session 
+from werkzeug.security import check_password_hash, generate_password_hash
 import os
 import glob
 
@@ -16,28 +17,97 @@ db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
-# Product Class/Model
-class Product(db.Model):
+# Song Class/Model
+class Song(db.Model):
+  __tablename__ = 'song'
   id = db.Column(db.Integer, primary_key=True)
-  path = db.Column(db.String(100), unique=True)
+  path = db.Column(db.String(300), unique=True)
   steps = db.Column(db.Integer)
 
   def __init__(self, path, steps):
     self.path = path
     self.steps = steps
 
-# Product Schema
-class ProductSchema(ma.Schema):
+# Song Schema
+class SongSchema(ma.SQLAlchemySchema):
   class Meta:
     fields = ('id', 'path', 'steps')
 
-# Init schema
-product_schema = ProductSchema()
-products_schema = ProductSchema(many=True)
+# User Class/Model
+class User(db.Model):
+  __tablename__ = 'user'
+  id = db.Column(db.Integer, primary_key=True)
+  user_name = db.Column(db.String(16), unique=True)
+  hash_password = db.Column(db.Integer)
+  email = db.Column(db.String(80), unique=True)
 
-# Create a Product
-@app.route('/product', methods=['POST'])
-def add_product():
+  def __init__(self, user_name, hash_password, email):
+    self.user_name = user_name
+    self.hash_password = hash_password
+    self.email = email
+
+# User Schema
+class UserSchema(ma.SQLAlchemySchema):
+  class Meta:
+    fields = ('id', 'user_name', 'hash_password', 'email')
+
+# UserSong Class/Model
+class UserSong(db.Model):
+  __tablename__ = 'usersong'
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+  song_id = db.Column(db.Integer, db.ForeignKey("song.id"))
+  rating = db.Column(db.Integer)
+
+  def __init__(self, user_id, song_id, rating):
+    self.user_id = user_id
+    self.song_id = song_id
+    self.rating = rating
+
+# UserSong Schema 
+class UserSongSchema(ma.SQLAlchemySchema):
+  class Meta:
+    fields = ('id', 'user_id', 'song_id', 'rating')
+
+# Init schema
+Song_schema = SongSchema()
+Songs_schema = SongSchema(many=True)
+
+User_schema = UserSchema()
+Users_schema = UserSchema(many=True)
+
+UserSong_schema = UserSongSchema()
+UserSongs_schema = UserSongSchema(many=True)
+
+# Register User
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  user_name = request.json['user_name']
+  email = request.json['email']
+  password = request.json['password']
+  user_name_exists = db.session.query(User.id).filter_by(user_name=user_name).first() is not None
+  email_exists = db.session.query(User.id).filter_by(email=email).first() is not None
+  if user_name_exists or email_exists:
+    return jsonify({'user_added': False})
+  hash_password = generate_password_hash(password)
+  new_user = User(user_name, hash_password, email)
+  db.session.add(new_user)
+  db.session.commit()
+  return jsonify({'user_added': True})
+
+# Sign In
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+  user_name_entered = request.json['user_name']
+  password_entered = request.json['password']
+  user = db.session.query(User).filter(User.user_name==user_name_entered).first()
+  if user is not None and check_password_hash(user.hash_password, password_entered):
+    return jsonify({'signed_in': True})
+  return jsonify({'signed_in': False})
+
+# Create a Song
+@app.route('/song', methods=['POST'])
+def add_Song():
   steps = request.json['steps']
   os.system("melody_rnn_generate \
     --config=basic_rnn \
@@ -51,44 +121,44 @@ def add_product():
   files = glob.glob(folder_path + file_type)
   max_file = max(files, key=os.path.getctime)
   path = max_file
-  new_product = Product(path, steps)
-  db.session.add(new_product)
+  new_Song = Song(path, steps)
+  db.session.add(new_Song)
   db.session.commit()
-  return product_schema.jsonify(new_product)
+  return Song_schema.jsonify(new_Song)
 
-# Get All Products
-@app.route('/product', methods=['GET'])
-def get_products():
-  all_products = Product.query.all()
-  result = products_schema.dump(all_products)
+# Get All Songs
+@app.route('/song', methods=['GET'])
+def get_Songs():
+  all_Songs = Song.query.all()
+  result = Songs_schema.dump(all_Songs)
   return jsonify(result)
 
-# Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-  product = Product.query.get(id)
-  return product_schema.jsonify(product)
+# Get Single Songs
+@app.route('/song/<id>', methods=['GET'])
+def get_Song(id):
+  song = Song.query.get(id)
+  return Song_schema.jsonify(song)
 
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-  product = Product.query.get(id)
+# Update a Song
+@app.route('/song/<id>', methods=['PUT'])
+def update_Song(id):
+  song = Song.query.get(id)
   path = request.json['path']
   steps = request.json['steps']
-  product.path = path
-  product.steps = steps
+  Song.path = path
+  Song.steps = steps
   db.session.commit()
-  return product_schema.jsonify(product)
+  return Song_schema.jsonify(song)
 
-# Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-  product = Product.query.get(id)
-  if os.path.exists(product.path):
-    os.remove(product.path)
-  db.session.delete(product)
+# Delete Song
+@app.route('/song/<id>', methods=['DELETE'])
+def delete_Song(id):
+  song = Song.query.get(id)
+  if os.path.exists(song.path):
+    os.remove(song.path)
+  db.session.delete(song)
   db.session.commit()
-  return product_schema.jsonify(product)
+  return Song_schema.jsonify(song)
 
 # Run Server
 if __name__ == '__main__':
