9758828491034f855261e8929c6ccd3274bde18a
jmhasty <jmhasty@iu.edu>
2022-02-08 23:52:23 -0500

    recreate Navbar component using MUI

--- react-front-end/src/components/Navbar/Navbar.js
+++ react-front-end/src/components/Navbar/Navbar.js
@@ -1,35 +1,100 @@
-import React from 'react';
+import * as React from 'react';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
 import logo from './logo.png';
 
-import {
-Nav,
-NavLink,
-NavMenu,
-NavBtn,
-NavBtnLink,
-} from './NavbarElements';
-
 const Navbar = () => {
-return (
-	<>
-	<Nav>
-		<NavMenu>
-			<NavLink to='/' activeStyle>
-				<img src={logo} alt='logo' />
-			</NavLink>
-			<NavLink to='/about' activeStyle>
-				About Us
-			</NavLink>
-			<NavLink to='/signUp' activeStyle>
-				Sign Up
-			</NavLink>
-		</NavMenu>
-		<NavBtn>
-		  <NavBtnLink to='/signIn'>Sign In</NavBtnLink>
-		</NavBtn>
-	</Nav>
-	</>
-);
-};
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+	const styles = {
+		  root: {
+			display: 'flex',
+		  },
+		  appBar: {
+			backgroundColor: '#44BBA4',
+		  },
+		  menuButton: {
+			marginLeft: 'auto',
+			marginRight: 36,
+			color: '#000000',
+			opacity: '1',
+		  },
+		  logo: {
+			marginLeft: 36,
+			marginRight: 'auto',
+			opacity: '1',
+		  },
+		  list: {
+			width: '15rem',
+		  },
+		  listItem: {
+			color: '#000000',
+			fontSize: '1.5rem',
+			display: 'flex',
+			alignText: 'center',
+		  },
+		  link: {
+			textDecoration: "none",
+		  },
+		};
+
+	return (
+		<Box style={styles.root}>
+			<AppBar position="static" style={styles.appBar}>
+				<Toolbar>
+					<IconButton style={styles.logo}>
+						<NavLink to ='/'>
+							<img src={logo} alt='logo' />
+						</NavLink>
+					</IconButton>
+					<IconButton 
+						style={styles.menuButton}
+						onClick={() => setIsDrawerOpen(true)}
+					>
+						<MenuIcon />						
+					</IconButton>
+					<Drawer 
+						open={isDrawerOpen} 
+						onClose={() => setIsDrawerOpen(false)} 
+						anchor='right'
+						PaperProps={{
+							sx: {
+							  backgroundColor: "#97B0AA"
+							}
+						  }}
+					>
+						<List style={styles.list}>
+							<NavLink to='/signUp' style={styles.link}>
+								<ListItem button style={styles.listItem}>
+									Sign Up
+								</ListItem>
+							</NavLink>
+							<NavLink to='/signIn' style={styles.link}>
+								<ListItem button style={styles.listItem}>
+									Sign In
+								</ListItem>
+							</NavLink>
+							<Divider />
+							<NavLink to='/about' style={styles.link}>
+								<ListItem button style={styles.listItem}>
+									About
+								</ListItem>
+							</NavLink>
+						</List>						
+					</Drawer>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+}
 
 export default Navbar;
