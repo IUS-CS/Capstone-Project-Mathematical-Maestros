fd50d866fc54e003247cd42fb0cea8b2dbab5f1a
jmhasty <jmhasty@iu.edu>
2022-04-26 00:03:01 -0400

    Update home page to call proper endpoint- was GET /song now is GET /api/song

--- react-front-end/src/pages/home.js
+++ react-front-end/src/pages/home.js
@@ -1,40 +1,37 @@
-import { useState, useEffect } from 'react'
-import Player from "../components/Player/Player"
+import { useState, useEffect } from "react";
+import Player from "../components/Player/Player";
 
 const Home = () => {
+  const [songs, setSongs] = useState([]);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
-    const [songs, setSongs] = useState([]);
-    const [currentSongIndex, setCurrentSongIndex] = useState(0);
-    const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
-  
-    // Modify the current state by setting the new data to
-    // the response from the backend
-    useEffect(()=>{
-      fetch('/song',{
-        'methods':'GET',
-        headers : {
-          'Content-Type':'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(response => setSongs(response))
-      .catch(error => console.log(error))
-  
-    }, [currentSongIndex])
-  
-    useEffect(() => {
-      setNextSongIndex(() => {
-        if (currentSongIndex + 1 > songs.length - 1) {
-          return 0;
-        } else {
-          return currentSongIndex + 1;
-        }
-      });
-    }, [currentSongIndex, songs.length]);
+  // Modify the current state by setting the new data to
+  // the response from the backend
+  useEffect(() => {
+    fetch("/api/song", {
+      methods: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setSongs(response))
+      .catch((error) => console.log(error));
+  }, [currentSongIndex]);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
 
   return (
     <div className="Home">
-      {/* <div className="weirdShape"></div> */}
       <Player
         currentSongIndex={currentSongIndex}
         setCurrentSongIndex={setCurrentSongIndex}
@@ -43,6 +40,6 @@ const Home = () => {
       />
     </div>
   );
-}
-  
+};
+
 export default Home;
